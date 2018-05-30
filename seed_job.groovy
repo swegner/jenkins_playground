9eff648a20622c4410e8ae0a8054d53c8e85ac3f
Scott Wegner <swegner@google.com>
2018-05-30 15:44:46 -0700

    Add some configuration

--- seed_job.groovy
+++ seed_job.groovy
@@ -1,1 +1,14 @@
-job('empty job')
+job('test_job_1') {
+  displayName('Test Job 1')
+  description('A job for testing.')
+  label('all_jobs')
+  scm {
+    github('swegner/jenkins_playground', 'master')
+  }
+  publishers {
+    git {
+      branch('https://github.com/swegner/jenkins_playground.git', 'job_output')
+      pushOnlyIfSuccess
+    }
+  }
+}
